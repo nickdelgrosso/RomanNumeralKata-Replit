168078a5fd7c96d5ab63679aee8779a26f4c61dc
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-09-04 18:06:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 
-# Directions:
-# Write a function, “from_roman()”, that takes any positive integer as input and returns the roman numeral string version of that integer as output.
-
 
 def from_roman():
   return None
 
 
-def test_roman_returns_none():
-  assert from_roman() is None
+def test_roman_1():
+  assert from_roman(1) == "I"
