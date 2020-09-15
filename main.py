8078a94f30997c7e7b5d3d2235f64197315f68c5
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-09-15 12:10:31 +0000

    Pre-filled in the tests with a parametrized version (to speed up TDD process for kata)

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import pytest
 
 
-def from_roman():
-  return None
+# Source Code:
+
+def from_roman(num: int) -> str:
+  return 'a'
 
 
-def test_roman_1():
-  assert from_roman(1) == "I"
+
+# Tests: 
+
+cases = [
+  (1, 'I'),
+  # (2, 'II'),
+  # (3, 'III'),
+]
+@pytest.mark.parametrize(['num', 'roman'], cases)
+def test_roman_1(num: int, roman: str):
+  assert from_roman(num) == roman
